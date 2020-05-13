68fb452932b889a3c6acccf419f0836a6952a5d5
Vivian Phu <viviana.phu@gmail.com>
2020-05-12 20:52:34 -0700

    step 2: check even num

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ function evenNum(){
     var num2 = parseInt(document.getElementById("num2").value);
 
     // check if the numbers fit criteria
-    if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100){
+    if (num1 <= 2 || num1 >= 100 && parseInt(num1,10) == isNaN){
+        document.getElementById("msg").innerHTML = "Number 1 input" + num1 + " is not a valid number.";
+        document.getElementById("result").innerHTML = "";
+    }
+    if (num2 <= 2 || num2 >= 100){
         document.getElementById("msg").innerHTML = "Invalid input, please try again.";
         document.getElementById("result").innerHTML = "";
     }
@@ -35,7 +39,7 @@ function calculateEvenNum(firstNum, secondNum){
         }
     }
     console.log(eNumArray);
-    document.getElementById("primeNumber").innerHTML = "There are " + eNumArray.length +" prime numbers.";
+    document.getElementById("result").innerHTML = "There are " + eNumArray.length +" even numbers:";
     document.getElementById("msg").innerHTML = eNumArray;
 }
 

07618b035f1c16ac1ad384ecd763c1b895291fb0
Vivian Phu <viviana.phu@gmail.com>
2020-05-12 20:43:06 -0700

    step 2: js functions

---
+++ app.js
@@ -0,0 +1,53 @@
+function evenNum(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    // check if the numbers fit criteria
+    if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100){
+        document.getElementById("msg").innerHTML = "Invalid input, please try again.";
+        document.getElementById("result").innerHTML = "";
+    }
+    else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100){
+        calculateEvenNum(num1,num2);
+    }
+}
+function calculateEvenNum(firstNum, secondNum){
+    var eNumArray = [];
+    var sNum = 0;
+    var bNum = 0;
+
+    if (firstNum > secondNum){
+        sNum = secondNum;
+        bNum = firstNum;
+    }
+    else if (firstNum < secondNum){
+        sNum = firstNum;
+        bNum = secondNum;
+    }
+
+    while(sNum <= bNum){
+        if(isEven(sNum) == true){
+            eNumArray.push(sNum);
+            sNum = sNum + 1;
+        }
+        else if (isEven(sNum) == false){
+            sNum = sNum + 1;
+        }
+    }
+    console.log(eNumArray);
+    document.getElementById("primeNumber").innerHTML = "There are " + eNumArray.length +" prime numbers.";
+    document.getElementById("msg").innerHTML = eNumArray;
+}
+
+function isEven(num) {
+    res = true;
+    for (var i=num; i<=num+1; i++){
+        if (num%2 == 0){
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
+}
